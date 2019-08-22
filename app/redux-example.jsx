d986e8953506c840cc7bb769185d6d3764b090ea
Cesare Ghirelli <cghirelli@arpae.it>
2019-08-22 13:46:12 +0200

    added pure functions example

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,39 @@
 var redux = require('redux');
 
 console.log('Starting redux example');
+
+// Pure function
+function add (a, b) {
+    return a + b;
+}
+
+// Inpure functions
+var a = 3;
+function add0(b) {
+    return a + b;
+}
+
+var result;
+function add1(a, b) {
+    result = a + b;
+    return result;
+}
+
+function add2(a, b) {
+    return a + b + new Date().getSeconds();
+}
+
+function changeprop(obj) {
+    return {
+        ...obj,
+        name: 'Jen'
+    }
+}
+var startingValue = {
+    name: 'Andrew',
+    age: 25
+}
+var res = changeprop(startingValue);
+
+console.log(startingValue);
+console.log(res);
