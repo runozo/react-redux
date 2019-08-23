f5c99029c297883e256c49cc331a196578e18e28
Cesare Ghirelli <cghirelli@arpae.it>
2019-08-23 11:12:20 +0200

    Add action for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,16 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+    name: 'Anonymous',
+    hobbies: [],
+    movies: []
+}
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
     // state = state || {name: 'Anonymous'};
 
     switch (action.type) {
@@ -10,6 +19,39 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
             return {
                 ...state,
                 name: action.name
+            }
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+            }
+        case 'REMOVE_HOBBY':
+            return {
+                ...state,
+                hobbies: state.hobbies.filter(hobby => hobby.id !== action.id)
+            }
+        case 'ADD_MOVIE':
+            return {
+                ...state,
+                movies: [
+                    ...state.movies,
+                    {
+                        id: nextMovieId++,
+                        title: action.title,
+                        genre: action.genre
+                    }
+                ]
+            }
+        case 'REMOVE_MOVIE':
+            return {
+                ...state,
+                movies: state.movies.filter(movie => movie.id !== action.id)
             }
         default:
             return state;
@@ -23,6 +65,7 @@ var unsubscribe = store.subscribe(() => {
 
     console.log('Name is', state.name);
     document.getElementById('app').innerHTML = state.name;
+    console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -36,9 +79,39 @@ store.dispatch({
     name: 'Andrew'
 });
 
-console.log('Name should be Andrew', store.getState())
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Running'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Walking'
+});
 
 store.dispatch({
     type: 'CHANGE_NAME',
     name: 'Emily'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Mad Max',
+    genre: 'Action'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Star Wars',
+    genre: 'Action'
+});
+
+store.dispatch({
+    type: 'REMOVE_MOVIE',
+    id: 1
+});
+
+store.dispatch({
+    type: 'REMOVE_HOBBY',
+    id: 2
 });
