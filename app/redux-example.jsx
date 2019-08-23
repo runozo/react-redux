92c35282a427b122cdcfb4dd06999b89bb451174
Cesare Ghirelli <cghirelli@arpae.it>
2019-08-23 10:46:03 +0200

    added devtools

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,9 +4,41 @@ console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
     // state = state || {name: 'Anonymous'};
-    return state;
+
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+            }
+        default:
+            return state;
+    }
 }
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// suscribe to changes
+var unsubscribe = store.subscribe(() => {
+    var state = store.getState();
+
+    console.log('Name is', state.name);
+    document.getElementById('app').innerHTML = state.name;
+});
+// unsubscribe();
+
+console.log('currentState', store.getState());
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Andrew'
+});
+
+console.log('Name should be Andrew', store.getState())
+
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Emily'
+});
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,8 +9,50 @@ var stateDefault = {
 }
 var reducer = (state = stateDefault, action) => {
     // state = state || {name: 'Anonymous'};
-    return state;
+    switch (action.type) {
+        case 'CHANGE_SEARCH_TEXT':
+            return {
+                ...state,
+                searchText: action.searchText
+            }
+        default:
+            return state;
+    }
 }
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// Subscribe to changes
+store.subscribe(() => {
+    var state = store.getState();
+    document.getElementById('app').innerHTML = state.searchText;
+});
+
 console.log('currentState', store.getState());
+
+// suscribe to changes
+// var unsubscribe = store.subscribe(() => {
+//     var state = store.getState();
+
+//     console.log('Name is', state.name)
+// });
+// unsubscribe();
+
+// console.log('currentState', store.getState());
+
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'work'
+});
+
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'dog'
+});
+
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'else'
+});
+
+console.log('searchText should be work', store.getState())
